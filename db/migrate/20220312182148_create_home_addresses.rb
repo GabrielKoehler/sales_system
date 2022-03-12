ed908ddf3f7fc960bafa806a6ccfc9d02fed3615
GabrielKoehler <gabriel.koehler@pro22.com.br>
2022-03-12 15:27:07 -0300

    created migrations

---
+++ db/migrate/20220312182148_create_home_addresses.rb
@@ -0,0 +1,17 @@
+class CreateHomeAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :home_addresses do |t|
+      t.string :street
+      t.string :neighborhood
+      t.integer :number
+      t.string :complement
+      t.string :postal_code
+      t.string :type
+      t.string :city
+      t.string :uf
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
